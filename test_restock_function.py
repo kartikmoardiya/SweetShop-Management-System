604f4064ff35cdba6c6b15e7861d93e58cc3ed38
Kartik Kishorbhai Moradiya <kartikmoradiya5964@gmail.com>
2025-07-17 20:02:12 +0530

    test f0r test restock successfull

---
+++ test_restock_function.py
@@ -0,0 +1,22 @@
+import unittest
+from operations_function import SweetOperator
+
+class TestSweetRestock(unittest.TestCase):
+
+    def setUp(self):
+        self.operator = SweetOperator()
+        # Add some sweets to the list
+        self.operator.add_sweet(1, "Kaju Katli", "chocolate", 50, 10)
+        self.operator.add_sweet(2, "Gulab Jamun", "candy", 40, 5)
+
+    def test_restock_successful(self):
+        result = self.operator.restock_sweets("Kaju Katli", 7)
+        self.assertEqual(result, "Restock Successful. 7 Kaju Katli(s) restocked. Current stock: 17")
+        # Quantity is updated
+        sweets = self.operator.view_sweets()
+        for sweet in sweets:
+            if sweet.name == "Kaju Katli":
+                self.assertEqual(sweet.quantity, 17)
+
+if __name__ == "__main__":
+    unittest.main(argv=['first-arg-is-ignored'], exit=False)
