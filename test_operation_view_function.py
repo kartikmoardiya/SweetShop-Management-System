89ce8a8011c12884264084ca28ffa7e06f136606
Kartik Kishorbhai Moradiya <kartikmoradiya5964@gmail.com>
2025-07-17 19:51:30 +0530

    test for view after adding one sweet with function

--- test_operation_view_function.py
+++ test_operation_view_function.py
@@ -10,5 +10,14 @@ class TestOperatorsView(unittest.TestCase):
         sweets = self.operator.view_sweets()
         self.assertEqual(sweets, [])   # Should be empty list when no sweets are added
 
+    def test_view_after_adding_one_sweet(self):
+        self.operator.add_sweet(1, "Kaju Katli", "chocolate", 50, 10)
+        sweets = self.operator.view_sweets()
+        self.assertEqual(len(sweets), 1)
+        self.assertEqual(sweets[0].name, "Kaju Katli")
+        self.assertEqual(sweets[0].category, "chocolate")
+        self.assertEqual(sweets[0].price, 50)
+        self.assertEqual(sweets[0].quantity, 10)
+
 if __name__ == "__main__":
     unittest.main(argv=['first-arg-is-ignored'], exit=False)
